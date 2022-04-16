44a3ca54b97d834846e4ff3eb1f1c5c42635307f
diksha6036 <malikdiksha385@gmail.com>
2022-04-16 16:03:55 +0530

    Print table

---
+++ table_printing_calculator.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+     int i,n,j,t;
+     j=1;
+     cout<<"Print Table of: ";
+     cin>>n;
+     i=n;
+     t=n;
+
+     while(i<=n*10 && j<=10){
+         cout<<t<<"*"<<j<<"="<<i<<endl;
+         i+=n;
+         j++;
+     }
+    return 0;
+}
